74b3ec011d55105bde2d72680dafb78f87158cdf
ChronSyn <scott@iocu.be>
2021-03-19 21:56:19 +0000

    Updated exports

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,1 +1,2 @@
 export { GenerateShades } from "./Shades";
+export { IArgs, TShadeDynamicKey, TOptionalShades, TOutputMode, TRequiredShades, TShadeVariants, IShades, IShadesResult, } from "./interface";
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 export { GenerateShades } from "./Shades";
+export {
+  IArgs,
+  TShadeDynamicKey,
+  TOptionalShades,
+  TOutputMode,
+  TRequiredShades,
+  TShadeVariants,
+  IShades,
+  IShadesResult,
+} from "./interface";
